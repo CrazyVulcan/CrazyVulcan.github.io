530d7809421b28955be53ed53aba0d2bea1bcd38
ComaToes <comatoes@blerf.net>
2015-07-26 19:27:49 +0100

    Fixed slowness due to tooltips
    Added cleanup destructor and removed tooltip generation on search
    results.

--- src/js/utopia.js
+++ src/js/utopia.js
@@ -371,29 +371,46 @@ module.directive( "tooltip", [ "$filter", function($filter) {
 		
 		scope: {
 			tooltip: "&",
-			tooltipPosition: "@"
+			tooltipPosition: "@",
+			tooltipShow: "="
 		},
 		
 		restrict: "A",
 		
 		link: function(scope,element,attrs) {
 			
-			$(element).data("powertipjq",$("<div></div>"));
-			$(element).powerTip({ placement: scope.tooltipPosition || 'ne-alt' });
+			var initialised = false;
 			
-			var icons = $filter("icons");
-
-			$(element).on( "powerTipRender", function() {
+			scope.$watch( "tooltipShow", function(show) {
 				
-				var div = $("<table class='card-tooltip'></table>");
+				if( show && !initialised ) {
+					
+					initialised = true;
+					
+					$(element).data("powertipjq",$("<div></div>"));
+					$(element).powerTip({ placement: scope.tooltipPosition || 'ne-alt' });
 				
-				$.each( scope.tooltip(), function(i,mod) {
-					div.append( "<tr><td>" + icons(mod.source) + "</td><td>" + (mod.value > 0 && i > 0 ? "+" : "") + mod.value + "</tr>" );
-				});
+					var icons = $filter("icons");
+						
+					$(element).on( "powerTipRender", function() {
+						
+						var div = $("<table class='card-tooltip'></table>");
+						
+						$.each( scope.tooltip(), function(i,mod) {
+							div.append( "<tr><td>" + icons(mod.source) + "</td><td>" + (mod.value > 0 && i > 0 ? "+" : "") + mod.value + "</tr>" );
+						});
+						
+						$("#powerTip").html(div);
+						
+					} );
+					
+					scope.$on("$destroy", function() {
+						$(element).powerTip("destroy");
+					});
+					
+				}
 				
-				$("#powerTip").html(div);
-				
-			} );
+			});
 			
 		}
 		
