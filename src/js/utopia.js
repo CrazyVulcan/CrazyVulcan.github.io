2986a8f09f4ac55c39cb2c1f5ad362d67ec168a3
ComaToes <comatoes@blerf.net>
2015-09-14 03:37:25 +0100

    Fixed zoom key in Firefox

--- src/js/utopia.js
+++ src/js/utopia.js
@@ -51,7 +51,7 @@ module.controller( "UtopiaCtrl", [ "$scope", "$filter", "cardLoader", "$factions
 	} );
 	
 	$scope.keypress = function(ev) {
-		if( ev.which == 26 ) {
+		if( ev.which == 26 || (ev.which == 122 && ev.ctrlKey) ) {
 			$scope.zoom = !$scope.zoom;
 			$scope.$broadcast("zoom",$scope.zoom);
 		}
