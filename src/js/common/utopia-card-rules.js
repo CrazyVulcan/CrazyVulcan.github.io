0ca88c0425b7777ee650ce5bbb23069d62982886
CrazyVulcan <butters1115@gmail.com>
2017-01-12 18:33:10 -0500

    Update utopia-card-rules.js

--- src/js/common/utopia-card-rules.js
+++ src/js/common/utopia-card-rules.js
@@ -5269,29 +5269,38 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 		//iks Toral, rules for the Duras Sisters
 		"crew:betor_crew_72282p": {
 			canEquip: function(upgrade,captain,fleet) {
-				ship: {
-					skill: function(card,ship,fleet,skill) {
-						if( card == ship.captain )
-							return resolve(card,ship,fleet,skill) + ( hasFaction(card,"klingon",ship,fleet) ? 4 : 1 );
-						return skill;
-					}
-				},				
+						
 				if( captain.class == "Lursa" )
 					return false;
 				}
-
-			},			
-		"crew:lursa_crew_72282p": {
-			canEquip: function(upgrade,captain,fleet) {
+			intercept: {
 				ship: {
 					skill: function(card,ship,fleet,skill) {
 						if( card == ship.captain )
-							return resolve(card,ship,fleet,skill) + ( hasFaction(card,"klingon",ship,fleet) ? 4 : 1 );
+							return resolve(card,ship,fleet,skill) + ( hasFaction(card,"klingon",ship,fleet) ? 3 : 1 );
 						return skill;
 					}
-				},				
+				}
+			}
+
+		},
+		
+		"crew:lursa_crew_72282p": {
+			canEquip: function(upgrade,captain,fleet) {
+						
 				if( captain.class == "B'Etor" )
 					return false;
 				}
-			}					
+			intercept: {
+				ship: {
+					skill: function(card,ship,fleet,skill) {
+						if( card == ship.captain )
+							return resolve(card,ship,fleet,skill) + ( hasFaction(card,"klingon",ship,fleet) ? 3 : 1 );
+						return skill;
+					}
+				}
+			}
+
+		}
+			
 }]);
