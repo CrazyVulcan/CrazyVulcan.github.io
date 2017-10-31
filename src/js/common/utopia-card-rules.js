134dcf696c22106716d2e55b61a4d392eca25266
CrazyVulcan <butters1115@gmail.com>
2017-10-31 19:55:19 -0400

    a

--- src/js/common/utopia-card-rules.js
+++ src/js/common/utopia-card-rules.js
@@ -4947,18 +4947,16 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 			},
 			canEquip: onePerShip("Immersion Shielding"),
 			intercept: {
-				self: {
-					cost: function(card,ship,fleet,cost) {
-						if( ship && hasFaction(upgrade,"federation",ship,fleet) || hasFaction(upgrade,"bajoran",ship,fleet) || hasFaction(upgrade,"vulcan",ship,fleet) )
-							return resolve(card,ship,fleet,cost) + 3;
-						return cost;
-					}
-				},
 				ship: {
 					shields: function(card,ship,fleet,shields) {
 						if( card == ship )
 							return resolve(card,ship,fleet,shields) + 1;
 						return shields;
+					},
+					cost: function(card,ship,fleet,cost) {
+						if( ship && hasFaction(upgrade,"federation",ship,fleet) || hasFaction(upgrade,"bajoran",ship,fleet) || hasFaction(upgrade,"vulcan",ship,fleet) )
+							return resolve(card,ship,fleet,cost) + 3;
+						return cost;
 					}
 				}
 			},
