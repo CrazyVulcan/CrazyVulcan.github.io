7822b103d6bff333846f44821790aebfb08d6f93
CrazyVulcan <butters1115@gmail.com>
2017-11-29 12:44:19 -0500

    a

--- src/js/common/utopia-card-rules.js
+++ src/js/common/utopia-card-rules.js
@@ -7377,18 +7377,8 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 	// RESOURCES
 		
 		"resource:fleet_commander": {
-			slotType: "ship_resource",
+			slotType: "ship-resource",
 			hideCost: true,
-			showShipResourceSlot: function(card,ship,fleet) {
-				if( ship.resource && ship.resource.type == "captain" )
-					return true;
-				var show = true;
-				$.each( fleet.ships, function(i,ship) {
-					if( ship.resource )
-						show = false;
-				} );
-				return show;
-			},
 			onRemove: function(resource,ship,fleet) {
 				$.each( fleet.ships, function(i,ship) {
 					if( ship.resource )
