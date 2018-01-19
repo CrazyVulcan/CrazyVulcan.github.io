b47f41969e9e742712af905a9a5db7612e86ef00
CrazyVulcan <butters1115@gmail.com>
2018-01-19 14:21:35 -0500

    a

--- src/data/captains.js
+++ src/data/captains.js
@@ -2820,10 +2820,10 @@ module.exports = [{
 	cost: 5,
 	skill: 8,
 	talents: 1,
-	text: "<b>SETUP::</b>\nPlace 3 Gold-Pressed Latinum Tokens (GPLT) on this card.\n-----------------------------\n<b>PLANNING PHASE:</b> Spend 1 GPLT and target a Captain equipped to another ship.\n\nIncrease or decrease the target Captains Captain Skill by 2 this game round.",
+	text: "<b>SETUP:</b>\nPlace 3 Gold-Pressed Latinum Tokens (GPLT) on this card.\n-----------------------------\n<b>PLANNING PHASE:</b> Spend 1 GPLT and target a Captain equipped to another ship. Increase or decrease the target Captains Captain Skill by 2 this game round.",
 	unique: true,
 	range: "1 - 3",
-	factions: ["federation"]
+	factions: ["ferengi"]
 }, {
 	type: "captain",
 	id: "picard_2017core",
--- src/data/upgrades.js
+++ src/data/upgrades.js
@@ -20,6 +20,15 @@ module.exports = [{
 	factions: ["independent"]
 },  {
 	type: "crew",
+	id: "par_lenor_75003",
+	set: ["75003"],
+	name: "Par Lenor",
+	cost: 3,
+	text: "WHEN THIS SHIP WOULD BE DESTROYED BY AN ATTACK: Equip this card to the attacking ship, even if it exceeds that ships restrictions with a number of GPLT on this card equal the the number of GPLT on the captain on this ship.\n-----------------------------------\n<b>ACTIVATION PHASE:</b> The opposing player may spend 1 GPLT on this card. Place 1 Disabled Token on an Upgrade equipped to this ship.",
+	unique: true,
+	factions: ["ferengi"]
+},  {
+	type: "crew",
 	id: "grilka_75003",
 	set: ["75003"],
 	name: "Grilka",
