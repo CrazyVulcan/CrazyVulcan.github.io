a45bcc56177f1b963285c4495d1487dc122094f9
CrazyVulcan <butters1115@gmail.com>
2018-02-26 16:11:20 -0500

    dfg

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -7678,15 +7678,15 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 					type: ["talent"]
 				}, {
 					type: ["crew"], 
-					rules: "Cost of 3sp or less",
+					rules: "*Cost of 3sp or less",
 					intercept: {
 						ship: {
 							cost: function() { return 0; },
 							canEquip: function(card,ship,fleet,canEquip) {
 								if( (valueOf(card,"cost",ship,fleet) <= 4) || $factions.hasFaction( ship, "federation", ship, fleet ) || $factions.hasFaction( ship, "klingon", ship, fleet ) || $factions.hasFaction( ship, "romulan", ship, fleet ) || $factions.hasFaction( ship, "dominion", ship, fleet ) || $factions.hasFaction( ship, "borg", ship, fleet ) || $factions.hasFaction( ship, "bajoran", ship, fleet ) || $factions.hasFaction( ship, "vulcan", ship, fleet ) || $factions.hasFaction( ship, "mirror-universe", ship, fleet ) )
 									return canEquip;
-	//							else ( (valueOf(card,"cost",ship,fleet) <= 3) )
-	//								return canEquip;
+								else ( (valueOf(card,"cost",ship,fleet) <= 3) )
+									return canEquip;
 								return false;
 							}
 						}
