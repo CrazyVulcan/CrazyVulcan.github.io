18be397b47d3438f67be6ee0fd5a8320cdc979d4
CrazyVulcan <butters1115@gmail.com>
2018-03-06 10:46:08 -0500

    a

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -6309,13 +6309,13 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 				ship:{
 					cost: function(card,ship,fleet,cost) {
 					modifier = 0;
-					
-					if ( card == ship || ship.class == "Borg Sphere" )
+						
+					if (ship && ship.class == "Borg Sphere" )
 					modifier = 15;
-					
+						
 					else modifier = 10;
-						return resolve(card,ship,fleet,cost) - modifier ;
-					return cost;
+						
+					return ship.cost - modifier;
 					}
 				}
 			}
