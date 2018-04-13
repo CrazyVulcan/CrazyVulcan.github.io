c56a2fd6eca31c62b972968edeae4f35d8f24ebc
CrazyVulcan <butters1115@gmail.com>
2018-04-13 00:26:47 -0400

    This cant work?

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -7824,7 +7824,9 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 							canEquip: function(card,ship,fleet,canEquip) {
 								if( (valueOf(card,"cost",ship,fleet) <= 4) && $factions.hasFaction( ship, "federation", ship, fleet ) || $factions.hasFaction( ship, "klingon", ship, fleet ) || $factions.hasFaction( ship, "romulan", ship, fleet ) || $factions.hasFaction( ship, "dominion", ship, fleet ) || $factions.hasFaction( ship, "borg", ship, fleet ) || $factions.hasFaction( ship, "bajoran", ship, fleet ) || $factions.hasFaction( ship, "vulcan", ship, fleet ) || $factions.hasFaction( ship, "mirror-universe", ship, fleet ) )
 									return canEquip;
-								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "independent", ship, fleet ) || $factions.hasFaction( ship, "ferengi", ship, fleet ) || $factions.hasFaction( ship, "kazon", ship, fleet ) || $factions.hasFaction( ship, "xindi", ship, fleet ))
+								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "independent", ship, fleet ) )
+									return canEquip;
+								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "ferengi", ship, fleet ) || $factions.hasFaction( ship, "kazon", ship, fleet ) || $factions.hasFaction( ship, "xindi", ship, fleet ))
 									return canEquip;
 								return false;
 				}}}},
@@ -7837,7 +7839,9 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 							canEquip: function(card,ship,fleet,canEquip) {
 								if( (valueOf(card,"cost",ship,fleet) <= 4) && $factions.hasFaction( ship, "federation", ship, fleet ) || $factions.hasFaction( ship, "klingon", ship, fleet ) || $factions.hasFaction( ship, "romulan", ship, fleet ) || $factions.hasFaction( ship, "dominion", ship, fleet ) || $factions.hasFaction( ship, "borg", ship, fleet ) || $factions.hasFaction( ship, "bajoran", ship, fleet ) || $factions.hasFaction( ship, "vulcan", ship, fleet ) || $factions.hasFaction( ship, "mirror-universe", ship, fleet ) )
 									return canEquip;
-								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "independent", ship, fleet ) || $factions.hasFaction( ship, "ferengi", ship, fleet ) || $factions.hasFaction( ship, "kazon", ship, fleet ) || $factions.hasFaction( ship, "xindi", ship, fleet ))
+								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "independent", ship, fleet ) )
+									return canEquip;
+								else if ( (valueOf(card,"cost",ship,fleet) <= 3) && $factions.hasFaction( ship, "ferengi", ship, fleet ) || $factions.hasFaction( ship, "kazon", ship, fleet ) || $factions.hasFaction( ship, "xindi", ship, fleet ))
 									return canEquip;
 								return false;
 				}}}}
