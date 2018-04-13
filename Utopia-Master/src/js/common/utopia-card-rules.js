de663e4c2234980f44821a6637ddc4a396317629
CrazyVulcan <butters1115@gmail.com>
2018-04-12 23:36:58 -0400

    CA

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -7826,7 +7826,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 									return 0;
 							return factionPenalty;},
 							canEquip: function(card,ship,fleet,canEquip) {
-								if( (valueOf(card,"cost",ship,fleet) <= 3) )
+								if( isUpgrade(card) && (valueOf(card,"cost",ship,fleet) <= 3) )
 									return canEquip;
 								return false;
 				}}}},
@@ -7841,8 +7841,9 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 									return 0;
 							return factionPenalty;},
 							canEquip: function(card,ship,fleet,canEquip) {
-								if( (upgrade.cost <= 3) )
+								if(  isUpgrade(card) && (valueOf(card,"cost",ship,fleet) <= 3) )
 									return canEquip;
+								else if( (valueOf(card,"cost",ship,fleet) <= 3) && $faction.hasFaction(ship,"federation",ship,fleet) )
 								return false;
 				}}}}
 			]
