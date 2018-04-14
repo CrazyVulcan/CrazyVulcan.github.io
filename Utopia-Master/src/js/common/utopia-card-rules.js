bc282374ef6f9d1db9b7e083f09dc495053736bf
CrazyVulcan <butters1115@gmail.com>
2018-04-14 15:41:33 -0400

    Lest not make the ship = 4sp

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -6239,7 +6239,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 					// Discounting up to 3 Upgrades that cost 5 or 6 sp
 					cost: function(upgrade,ship,fleet,cost) {
 						var matching = 0;					
-						if( upgrade.cost == 5 || upgrade.cost == 6 && matching <= 3){
+						if( upgrade.cost == 5 || upgrade.cost == 6 && matching <= 3 && isUpgrade){
 						//Each time there is a match add +1 to "matching"
 						matching = matching + 1; }
 							return 4;
