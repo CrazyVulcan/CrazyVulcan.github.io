2c748116a7334ccf11cc23a1c6659e0861e360b4
CrazyVulcan <butters1115@gmail.com>
2018-09-04 12:07:34 -0400

    -

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -8274,7 +8274,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 				ship: {
 					// Add the "crew" type to the Captain slot
 					type: function(card,ship,fleet,type) {
-						if( $.inArray("captain",type) >= 0 )
+						if( $.inArray("captain",type) )
 							return type.concat(["crew"]);
 						return type;
 					}
