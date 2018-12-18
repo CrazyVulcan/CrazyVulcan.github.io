d309aa038965ea3658d0bdd36e07ae7a1f2a49e8
CrazyVulcan <butters1115@gmail.com>
2018-12-18 05:00:08 -0500

    -I think I may have it

--- Database/src/js/common/utopia-card-loader.js
+++ Database/src/js/common/utopia-card-loader.js
@@ -4,7 +4,7 @@ module.factory( "cardLoader", [ "$http", "$filter", "cardRules", "$factions", fu
 
 	var valueOf = $filter("valueOf");
 
-	return function(cards, sets, missionSets, shipClasses, token, callback) {
+	return function(cards, sets, missionSets, missions, shipClasses, token, callback) {
 
 		function isDuplicate(card, cards) {
 			var dupe = false;
