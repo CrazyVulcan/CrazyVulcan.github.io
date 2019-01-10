b9b123988c36cb01c481f79b78cfd0cc35c19190
CrazyVulcan <butters1115@gmail.com>
2019-01-10 13:24:29 -0500

    -

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -4791,7 +4791,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 				}
 			},
 			canEquip: function(upgrade,ship,fleet) {
-				if( ship.classData && ship.classData.rearArc  || ship.upgrade.name == "Federation" )
+				if( ship.classData && ship.classData.rearArc  || ( ship && ship.upgrade.name == "Federation" ) )
 					return false;
 				return ship.hull >= 4;
 			},
