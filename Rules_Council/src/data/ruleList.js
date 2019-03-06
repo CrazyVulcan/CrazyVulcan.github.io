50a8960ece398dda4e04759f5da5f53dee649e60
CrazyVulcan <butters1115@gmail.com>
2019-03-06 14:41:20 -0500

    -
    A html page for each ruling would be faster and alow for more customazastion then a straight css JSON drivin page

---
+++ Rules_Council/src/data/ruleList.js
@@ -0,0 +1,15 @@
+module.exports = [{
+	type: "rule",
+	ruleName: "GOLDEN RULES",
+	index: "01.0",
+	rulingSet: "INTRODUCTION"
+}, {
+	type: "rule",
+	name: "CARD INTERPRETATION",
+	index: "01.2",
+	parentSet: "INTRODUCTION"
+}];
+
+
+
+
