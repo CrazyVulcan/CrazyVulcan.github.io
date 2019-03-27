19f99cb513d73c48ec9cd1d010a2a0b4510e0dd9
CrazyVulcan <butters1115@gmail.com>
2019-03-26 21:17:46 -0400

    -

--- Utopia-Master/src/data/index.js
+++ Utopia-Master/src/data/index.js
@@ -3,8 +3,6 @@ var path = require("path");
 
 var data = {
 	sets: require("./sets"),
-	missionSets: require("./missionSets"),
-	missions: require("./missions"),
 	ships: require("./ships"),
 	shipClasses: require("./ship_classes"),
 	captains: require("./captains"),
