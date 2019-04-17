455b1c115a327c607136d0628729593278e525f8
CrazyVulcan <butters1115@gmail.com>
2019-04-17 16:02:04 -0400

    -

--- Utopia-Master/src/js/common/utopia-card-rules.js
+++ Utopia-Master/src/js/common/utopia-card-rules.js
@@ -7849,7 +7849,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 				ship: {
 					// Add the "crew" type to all Tech and Borg slots
 					type: function(card,ship,fleet,type) {
-						if( ship.hasFaction == "borg" && ( $.inArray("tech",type) >= 0 || $.inArray("borg",type) >= 0 ) )
+						if( $.inArray("tech",type) >= 0 || $.inArray("borg",type) >= 0 && ship.hasFaction == "borg" )
 							return type.concat(["crew"]);
 						return type;
 					}
