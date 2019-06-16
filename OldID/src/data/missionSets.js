ab8638b4237e10b0566aa90c6c7c92ab88161769
CrazyVulcan <butters1115@gmail.com>
2019-06-16 15:20:33 -0400

    -

---
+++ OldID/src/data/missionSets.js
@@ -0,0 +1,11 @@
+module.exports = [{
+	type: "missionSet",
+	sourceID: "001",
+	name: "A New Source Of Dilithium",
+	orderDate: "2000-01-01"
+}, {
+	type: "missionSet",
+	sourceID: "002",
+	name: "The Chase",
+	orderDate: "2000-01-02"
+}];
