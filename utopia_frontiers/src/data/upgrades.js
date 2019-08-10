3b61734f9552fc6fb439f6dc5b3f642a6b9d9bf7
CrazyVulcan <butters1115@gmail.com>
2019-08-10 18:19:14 -0400

    -

--- utopia_frontiers/src/data/upgrades.js
+++ utopia_frontiers/src/data/upgrades.js
@@ -1,11 +1,12 @@
 module.exports = [{
-	type: "type",
-	id: "X000",
-	set: ["00000"],
-	name: "Upgrade Name",
-	cost: 0,
-	text: "",
-	unique: false,
-	factions: [""]
+	type: "question",
+	id: "Q001",
+	set: ["Fed001"],
+	name: "Emergency Saucer Seperation",
+	cost: 3,
+	text: "<b>ACTION:</b> Place a [crew] Upgrade card under this card and set it beside your play area.\n\nYou may discard up to two damage cards equiped to this ship and your agility is now 3. Reduce the Dificulty of all turns on this ships dial.\n(<i>Galaxy Class Only</i>)",
+	OnePerShip: true,
+	shipLimit: true,
+	factions: ["federation"]
 }];
 
