9215cd3c97df7b73fefcee89e1e33856d2ebe315
CrazyVulcan <butters1115@gmail.com>
2019-08-10 09:37:52 -0400

    -

--- utopia_frontiers/src/data/sets.js
+++ utopia_frontiers/src/data/sets.js
@@ -4,4 +4,10 @@ module.exports = [{
 	name: "Updated Old Cards",
 	releaseDate: "2019-08-01",
 	parentSet: "Old Cards 01"
+},{
+	type: "set",
+	id: "Fed001",
+	name: "Galaxy Class",
+	releaseDate: "2019-08-02",
+	parentSet: "Old Cards"
 }];
