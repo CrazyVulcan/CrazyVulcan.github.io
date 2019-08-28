e6e45932ae60bec524a94c6c9e9484a577f5020b
CrazyVulcan <butters1115@gmail.com>
2019-08-27 21:52:46 -0400

    -

--- Master/src/js/common/utopia-card-rules.js
+++ Master/src/js/common/utopia-card-rules.js
@@ -8927,7 +8927,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 
 						// Count the number of empty upgrade slots
 						$.each( $filter(ship.upgrades)(ship), function(i, slot) {
-							if( slot.occupant == null && slot.type != "talent") {
+							if( slot.occupant == null && card.type != "talent") {
 								// For Each count suptract form cost.
 								candidates = candidates + 1;
 								}
